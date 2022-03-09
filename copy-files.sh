2408a2bfebaa989ff9f5f3f833baff3c608dc695
Dmitry Avtonomov <dmitry.avtononomov@bertis.com>
2022-03-08 16:00:18 -0800

    Add bash script to copy needed dotfiles

--- copy-files.sh
+++ copy-files.sh
@@ -4,16 +4,30 @@ if [ $# -eq 0 ]
     exit
 fi
 
-echo "Args to [$0]=[$#], Destination: [$1]"
+dest="$1"
+echo "Args to [$0]=[$#], Destination: [$dest]"
 
 # Reminder how to read output of a command to an array in bash
 # paths=()
 readarray -d $'\0' paths < <(find . -maxdepth 1 -name "\.*" -type f -print0)
 for p in ${paths[@]}; do
-	echo "To copy: [$p]"
+	echo "To copy: [$p] -> $dest"
 done
 
-dest="$1"
-find . -maxdepth 1 -type f -name "\.*" | xargs -I{} cp {} $dest
+read -p "Are you sure (y/n)? " -n 1 -r SURE
+echo
+#case "$SURE" in
+#    y|Y ) echo "Copying...";;
+#    n|N ) echo "Cancelled";;
+#    * ) echo "Invalid choice, cancelled";;
+#esac
 
+if [[ $SURE =~ ^[Yy]$ ]]
+then
+    # do dangerous stuff
+    echo "Copying..."
+    find . -maxdepth 1 -type f -name "\.*" | xargs -I{} cp {} $dest
+else
+    echo "Cancelled"
+fi
 
