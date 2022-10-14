c87d1b6d0e841ba4adcd1ca73e4f96d303566cf5
Dmitry Avtonomov <dmitry.avtonomov@bertis.com>
2022-10-14 11:45:10 -0700

    Update powershell profile to include ssh-agent startup

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,18 +1,28 @@
 # Set tab completion to work like in Bash (complete up to longest common sequence)
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 
+
 # Autocompletion for git
 # Requires: PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
 Import-Module posh-git
 
+
+# Ssh key automatic loading
+# Requires: PowerShellGet\Install-Module posh-sshell -Scope CurrentUser -Force
+# Starting module SshAgent requires that 'ssh-agent' service is 'Enabled'
+# From an elevated prompt: Get-Service ssh-agent | Set-Service -StartupType Manual
+Import-Module posh-sshell
+Start-SshAgent
+
+
 Set-PSReadlineOption -BellStyle None
 
 # Starship (alternative to Oh my posh)
-Invoke-Expression (&starship init powershell)
+#Invoke-Expression (&starship init powershell)
 
 
 # Oh my posh
-#oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh init pwsh | Invoke-Expression
 # Set the theme for oh-my-posh
 # Requires: Install-Module oh-my-posh -Scope AllUsers
 #Import-Module oh-my-posh
