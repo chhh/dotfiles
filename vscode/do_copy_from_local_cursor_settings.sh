fee51be4d431cb17bc3a8b7a6ec91c531f76e2f7
Dmitry Avtonomov <dmt.avt@gmail.com>
2026-02-18 21:38:12 -0800

    Add scripts to copy Cursor and VSCode configs on Mac

---
+++ vscode/do_copy_from_local_cursor_settings.sh
@@ -0,0 +1,2 @@
+cp ~/Library/Application\ Support/Cursor/User/keybindings.json ~/src/dotfiles/vscode/
+cp ~/Library/Application\ Support/Cursor/User/settings.json ~/src/dotfiles/vscode/ 
---
+++ vscode/do_copy_from_local_vscode_settings.sh
@@ -0,0 +1,2 @@
+cp ~/Library/Application\ Support/Code/User/keybindings.json ~/src/dotfiles/vscode/
+cp ~/Library/Application\ Support/Code/User/settings.json ~/src/dotfiles/vscode/ 
